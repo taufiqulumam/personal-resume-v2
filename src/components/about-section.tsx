cae7d9d58d98b24db7f74caaeb7c7ff3687bfa85
Muhammad Taufiqul Umam <taufiqulmam@gmail.com>
2025-08-07 15:51:54 +0700

    fix typo

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Mail, MapPin } from 'lucide-react'
 import { Badge } from "./ui/badge"
 
 export function AboutSection() {
